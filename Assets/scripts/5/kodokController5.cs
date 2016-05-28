1a904f3ff0fedd68d5ee00ae3a189bec1609d88f
rezaramadhan <rezaramadhan.m@gmail.com>
2016-05-28 18:58:37 +0700

    Add SFX for page 2 & 5

--- Assets/scripts/5/kodokController5.cs
+++ Assets/scripts/5/kodokController5.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class kodokController5 : MonoBehaviour {
     public float incPosXY;
     public bool isShouldJump = false;
+	private AudioSource jumpSFX;
     // Use this for initialization
     void Start () {
-	
+		jumpSFX = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +18,7 @@ public class kodokController5 : MonoBehaviour {
     }
 
     void jump() {
+		jumpSFX.Play ();
         Vector2 currentPos = transform.position;
         transform.position = new Vector2(currentPos.x + incPosXY, currentPos.y + incPosXY);
         if (transform.position.x >= 15) {
